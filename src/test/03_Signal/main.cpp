3635bbcdd3d2b47efd7546b55b0207a7c96fce3b
John <yangtianzhuo970425@gmail.com>
2024-12-29 00:38:33 +1300

    Update Signal and Visitor classes to replace FuncTraits with Func and simplify connection handling in tests

--- src/test/03_Signal/main.cpp
+++ src/test/03_Signal/main.cpp
@@ -10,13 +10,8 @@ using namespace My;
 using namespace std;
 
 int main() {
-  Signal<float, int&, int> s0;
-  auto connection = s0.Connect([](float, int& n) { n = 3; });
-  int a = 2;
-  s0.Emit(1.f, a, 3);
-  cout << a << endl;
-  s0.Disconnect(std::move(connection));
-  a = 4;
-  s0.Emit(1.f, a, 3);
-  cout << a << endl;
+  Signal<int, int> mouseMoved;
+  auto connection =
+      mouseMoved.Connect([](int x, int y) { cout << x << ", " << y << endl; });
+  mouseMoved.Emit(3, 4);
 }
